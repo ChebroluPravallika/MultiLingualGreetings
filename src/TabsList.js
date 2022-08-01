0e9cfc8ad52a2975dc8edd97187c3db662a1cc81
ChebroluPravallika <95267108+ChebroluPravallika@users.noreply.github.com>
2022-08-01 20:34:10 +0530

    Update TabsList.js

--- src/TabsList.js
+++ src/TabsList.js
@@ -2,29 +2,13 @@ import './App.css'
 
 const TabsList = props => {
   const {each, onClickTabItem} = props
-  let clsName = ''
   const {buttonText, id} = each
   const onClickBtn = () => {
-    clsName = 'clsBtn'
     onClickTabItem(id)
   }
   return (
     <li style={{listStyle: 'none'}}>
-      <button
-        type="button"
-        onClick={onClickBtn}
-        className={clsName}
-        style={{
-          backgroundColor: 'white',
-          color: 'red',
-          margin: '10px',
-          width: '100px',
-          height: '40px',
-          borderRadius: '20px',
-          border: '1px solid red',
-          cursor: 'pointer',
-        }}
-      >
+      <button type="button" onClick={onClickBtn} className="Btn">
         {buttonText}
       </button>
     </li>
